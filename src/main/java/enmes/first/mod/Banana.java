d42a871820ac9072ddd30581183133053b29da87
enzomescall <enmes.mescall@gmail.com>
2022-02-09 20:32:59 -0500

    added tooltip to banana

--- src/main/java/enmes/first/mod/Banana.java
+++ src/main/java/enmes/first/mod/Banana.java
@@ -1,12 +1,17 @@
 package enmes.first.mod;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+
+import java.util.*;
 
 public class Banana extends Item {
 
@@ -20,4 +25,11 @@ public class Banana extends Item {
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
     }
 
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+
+        // default white text
+        tooltip.add( new TranslatableText("just a banana") );
+
+    }
 }
